afcd13162b67f37e5bac321938fe112f6d02d90d
GudipatiAshokreddy <gudipatiashok205@gmail.com>
2019-05-10 18:03:17 +0530

    first commit

---
+++ testiOS/RSSReader/TopMediaController.swift
@@ -0,0 +1,57 @@
+/*
+ //
+ //  AppDelegate.swift
+ //  TestApp
+ //
+ //  Created by Ashok on 10/05/19.
+ //  Copyright © 2019 Ashok. All rights reserved.
+ //
+ */
+
+import UIKit
+
+class TopMediaController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var artistLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBInspectable var feedURL: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let request = URLRequest(url: URL(string: feedURL)!)
+        NSURLConnection.sendAsynchronousRequest(request, queue: OperationQueue.main) { response, data, error in
+            if let jsonData = data,
+                let feed = (try? JSONSerialization.jsonObject(with: jsonData, options: .mutableContainers)) as? NSDictionary,
+                let title = feed.value(forKeyPath: "feed.entry.im:name.label") as? String,
+                let artist = feed.value(forKeyPath: "feed.entry.im:artist.label") as? String,
+                let imageURLs = feed.value(forKeyPath: "feed.entry.im:image") as? [NSDictionary] {
+                    if let imageURL = imageURLs.last,
+                        let imageURLString = imageURL.value(forKeyPath: "label") as? String {
+                            self.loadImageFromURL(URL(string:imageURLString)!)
+                    }
+                self.titleLabel.text = title
+                self.titleLabel.isHidden = false
+                self.artistLabel.text = artist
+                self.artistLabel.isHidden = false
+                
+            }
+        }
+    }
+    
+    func loadImageFromURL(_ URL: Foundation.URL) {
+        let request = URLRequest(url: URL)
+        NSURLConnection.sendAsynchronousRequest(request, queue: OperationQueue.main) { response, data, error in
+            if let imageData = data {
+                self.imageView.image = UIImage(data: imageData)
+            }
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+        
+}
